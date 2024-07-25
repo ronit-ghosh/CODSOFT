e1e7b525ac9dd3e81a9e0e6fe6e05f027929e787
ronit-ghosh <ronitghosh06@gmail.com>
2024-07-25 17:31:37 +0530

    Files Added

---
+++ Calculator/script.js
@@ -0,0 +1,23 @@
+const display = document.querySelector("#display");
+
+function displayChar(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Syntax Error";
+    setTimeout(() => {
+      display.value = "";
+    }, 1000);
+
+    if (display.endsWith("%")) {
+    }
+  }
+}
