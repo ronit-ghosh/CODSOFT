98bf95378ad63da8aadcbd9b414a95f5e8ed96e5
ronit-ghosh <ronitghosh06@gmail.com>
2024-07-25 15:51:48 +0530

    Files Added

--- Landing-Page/README.md
+++ Landing-Page/README.md
@@ -8,22 +8,15 @@ This is an animated website built using HTML, CSS, JavaScript, and the GSAP JS l
 - Responsive design
 - Interactive elements
 
-## Installation
-
-1. Clone the repository:
-    ```sh
-    git clone https://github.com/your-username/animated-website.git
-    ```
-2. Navigate to the project directory:
-    ```sh
-    cd animated-website
-    ```
-3. Open `index.html` in your browser.
-
 ## Technologies Used
 
 - HTML
 - CSS
 - JavaScript
-- GSAP Js Library
+- GSAP JS Library
+- Tilt JS
+
+## Installation
+
+- Open `index.html` in your browser
 
--- Landing-Page/script.js
+++ Landing-Page/script.js
@@ -1,113 +1,113 @@
-console.log("Hey It's me Ronit") 
+console.log("Hey It's me Ronit");
 
 // Moving circle
 document.addEventListener("mousemove", (dets) => {
-    cursor.style.left = dets.x + "px"
-    cursor.style.top = dets.y + "px"
-    blur_cursor.style.left = dets.x - 100 + "px"
-    blur_cursor.style.top = dets.y - 100 + "px"
+  cursor.style.left = dets.x + "px";
+  cursor.style.top = dets.y + "px";
+  blur_cursor.style.left = dets.x - 100 + "px";
+  blur_cursor.style.top = dets.y - 100 + "px";
+});
 
-})
-
+// Hover effect on Navbar
 document.querySelectorAll("nav h4").forEach((e) => {
-    e.addEventListener("mouseenter", () => {
-        cursor.style.scale = 3
-        cursor.style.border = "1px solid white"
-        cursor.style.backgroundColor = "transparent"
-        cursor.style.transition = "all ease-in-out .1s"
-    })
-    e.addEventListener("mouseleave", () => {
-        cursor.style.scale = 1
-        cursor.style.border = "none"
-        cursor.style.backgroundColor = "#95c11e"
-        cursor.style.transition = "none"
-    })
-})
+  e.addEventListener("mouseenter", () => {
+    cursor.style.scale = 3;
+    cursor.style.border = "1px solid white";
+    cursor.style.backgroundColor = "transparent";
+    cursor.style.transition = "all ease-in-out .1s";
+  });
+  e.addEventListener("mouseleave", () => {
+    cursor.style.scale = 1;
+    cursor.style.border = "none";
+    cursor.style.backgroundColor = "#95c11e";
+    cursor.style.transition = "none";
+  });
+});
 
-
+// GSAP codes
 gsap.to("nav", {
-    backgroundColor: "black",
-    height: "100px",
-    duration: 0.5,
-    scrollTrigger: {
-        trigger: "nav",
-        scroller: "body",
-        start: "top -10%",
-        end: "top -11%",
-        scrub: 2
-    }
-})
+  backgroundColor: "black",
+  height: "100px",
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "nav",
+    scroller: "body",
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 2,
+  },
+});
 
 gsap.to(".container", {
-    backgroundColor: "black",
-    duration: 1,
-    scrollTrigger: {
-        trigger: ".container",
-        scroller: "body",
-        start: "top -30%",
-        end: "top -70%",
-        scrub: 2
-    }
-})
+  backgroundColor: "black",
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".container",
+    scroller: "body",
+    start: "top -30%",
+    end: "top -70%",
+    scrub: 2,
+  },
+});
 
 gsap.from(".about_us", {
-    y: 90,
-    opacity: 0,
-    duration: 1,
-    scrollTrigger: {
-        trigger: ".about_us",
-        scroller: "body",
-        start: "top 85%",
-        end: "top 75%",
-        scrub: 2
-    }
-})
+  y: 90,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".about_us",
+    scroller: "body",
+    start: "top 85%",
+    end: "top 75%",
+    scrub: 2,
+  },
+});
 
 gsap.from(".section3", {
-    y: 100,
-    scale: 0.9,
-    opacity: 0,
-    duration: 1,
-    scrollTrigger: {
-        trigger: ".about_us",
-        scroller: "body",
-        start: "bottom 85%",
-        end: "bottom 75%",
-        scrub: 2
-    }
-})
+  y: 100,
+  scale: 0.9,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".about_us",
+    scroller: "body",
+    start: "bottom 85%",
+    end: "bottom 75%",
+    scrub: 2,
+  },
+});
 
 gsap.to(".colon1", {
-    y: 70,
-    x: 70,
-    scrollTrigger: {
-        trigger: ".colon1",
-        scroller: "body",
-        start: "bottom 50%",
-        end: "top 105%",
-        scrub: 3
-    }
+  y: 70,
+  x: 70,
+  scrollTrigger: {
+    trigger: ".colon1",
+    scroller: "body",
+    start: "bottom 50%",
+    end: "top 105%",
+    scrub: 3,
+  },
 });
 
 gsap.to(".colon2", {
-    y: -70,
-    x: -70,
-    scrollTrigger: {
-        trigger: ".colon2",
-        scroller: "body",
-        start: "bottom 110%",
-        end: "top 120%",
-        scrub: 3
-    }
+  y: -70,
+  x: -70,
+  scrollTrigger: {
+    trigger: ".colon2",
+    scroller: "body",
+    start: "bottom 110%",
+    end: "top 120%",
+    scrub: 3,
+  },
 });
 
 gsap.to(".section6 h1", {
-    y: -70,
-    scrollTrigger: {
-        trigger: ".section6 h1",
-        scroller: "body",
-        start: "top 70%",
-        end: "top 75%",
-        scrub: 3
-    }
-})
+  y: -70,
+  scrollTrigger: {
+    trigger: ".section6 h1",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 75%",
+    scrub: 3,
+  },
+});
